969b3014f6377ed934ecd99c595abbaeefb5c31c
darkwolfxj <hesitation.dotdot@gmail.com>
2020-02-27 18:41:34 -0500

    fixed populating update form

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -4,31 +4,36 @@ import { useRouteMatch, Redirect } from "react-router-dom";
 
 export default function MovieUpdate({ movieList, setMovieList, history }) {
     const match = useRouteMatch()
-    const [editValue, setEditValue] = useState({})
-    const handleChange = (e) => setEditValue({ ...editValue, [e.target.name]: `${ e.target.value }` })
-    const handleChangeArr = (e) => setEditValue({ ...editValue, stars: [e.target.value] })
+    const [movie, setMovie] = useState({})
+    const handleChange = (e) => setMovie({ ...movie, [e.target.name]: `${ e.target.value }` })
+    const handleChangeArr = (e) => setMovie({ ...movie, stars: [e.target.value] })
     const handleSubmit = (e) => {
         e.preventDefault()
         e.persist()
         axios
-            .put(`http://localhost:5000/api/movies/${ match.params.id }`, { id: match.params.id, title: `${ editValue.title }`, director: `${ editValue.director }`, metascore: JSON.parse(editValue.metascore), stars: editValue.stars })
+            .put(`http://localhost:5000/api/movies/${ match.params.id }`, { id: match.params.id, title: `${ movie.title }`, director: `${ movie.director }`, metascore: JSON.parse(movie.metascore), stars: movie.stars })
             .then(res => { 
                 setMovieList([...movieList.filter(i => i.id !== match.params.id) ,res.data])
                 console.log("movies", res.data)
             })
             history.push("/")
     }
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5000/api/movies/${ match.params.id }`)
+            .then(res => setMovie(res.data))
+    },[match.params.id])
     return (
         <div>
             <form onSubmit={ handleSubmit }>
                 <label htmlFor="title">Title:</label>
-                <input onChange={ handleChange } name="title" type="text" value={ editValue.title } placeholder="title" />
+                <input onChange={ handleChange } name="title" type="text" value={ movie.title } placeholder="title" />
                 <label htmlFor="director">Director:</label>
-                <input onChange={ handleChange } name="director" type="text" value={ editValue.director } placeholder="director" />
+                <input onChange={ handleChange } name="director" type="text" value={ movie.director } placeholder="director" />
                 <label htmlFor="metascore">Metascore:</label>
-                <input onChange={ handleChange } name="metascore" type="number" value={ editValue.metascore } placeholder="metascore" />
+                <input onChange={ handleChange } name="metascore" type="number" value={ movie.metascore } placeholder="metascore" />
                 <label htmlFor="stars">Stars:</label>
-                <textarea onChange={ handleChangeArr } name="stars" type="text" value={ editValue.stars } placeholder="stars" />
+                <textarea onChange={ handleChangeArr } name="stars" type="text" value={ movie.stars } placeholder="stars" />
                 <button type="submit">Save Changes</button>
             </form>
         </div>
